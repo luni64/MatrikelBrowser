50ce6eadb247605feb25c5862b9c1836617cc5ee
luni64 <lutz@niggl-schlagbauer.de>
2024-04-08 17:47:32 +0200

    cleanup

--- MatrikelBrowser/ViewModels/ItemViewModels/ItemVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ArchiveBrowser.ViewModels
-{
-    public class ItemVM : BaseViewModel
-    {
-        public int Indent { get; protected set; } = 0;
-        virtual public bool IsSelected { get; set; } = false;
-    }
-}
