355bba4ec905c06a3c43c1467f925f2d936edc76
luni64 <lutz@niggl-schlagbauer.de>
2025-01-07 17:59:09 +0100

    Add missing transcript in Event view model

--- MatrikelBrowser/ViewModels/BookmarkVMs/BirthEventVM.cs
+++ MatrikelBrowser/ViewModels/BookmarkVMs/BirthEventVM.cs
@@ -15,8 +15,9 @@ namespace MatrikelBrowser.ViewModels
         }
 
         public int ID { get => model.Id; }
-        public string Title { get => model.Title; set => SetProperty(model, e => e.Title, value); }
         public int SheetNr { get => model.SheetNr; set => SetProperty(model, e => e.SheetNr, value); }
+        public string Title { get => model.Title; set => SetProperty(model, e => e.Title, value); }
+        public string Transcript { get => model.Transcript; set => SetProperty(model, e => e.Transcript, value); }
         public double X { get => model.X; set => SetProperty(model, e => e.X, value); }
         public double Y { get => model.Y; set => SetProperty(model, e => e.Y, value); }
         public double W { get => model.W; set => SetProperty(model, e => e.W, value); }
